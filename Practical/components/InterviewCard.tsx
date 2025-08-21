7c099d0d0fc298ccb35e58d2f4520df0c9fb5c6c
SamsonGulma <sams0ngultek@gmail.com>
2025-08-21 06:58:05 +0000

    Aug 21, 2025, 9:58 AM

--- Practical/components/InterviewCard.tsx
+++ Practical/components/InterviewCard.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import dayjs from 'dayjs';
 import Image from 'next/image';
-import { getRandomInterviewCover } from '@/utils/utils';
+import { Button } from './ui/button';
+import { getRandomInterviewCover } from '@/lib/utils';
+import Link from 'next/dist/client/link';
+import DisplayTechIcons from './DisplayTechIcons';
 
-const InterviewCard = ({ InterviewId, userId, role, type, techstack,
+const InterviewCard = ({ interviewId, userId, role, type, techstack,
     level, questions, finalized, createdAt} : InterviewCardProps
 ) => {
     const feedback = null as Feedback | null;
@@ -18,7 +21,29 @@ const InterviewCard = ({ InterviewId, userId, role, type, techstack,
               {normalizedType}
             </p>
           </div>
-          <Image src={getRandomInterviewCover()} alt='Interview Cover' width={90} height={90} className='rounded-full object-fit size-[90px]'/>
+          <Image src={getRandomInterviewCover()} alt='Interview Cover' width={90} height={90} className='rounded-full object-fit size-[90px]' />
+          <h3 className='mt-5 capitalize'> {role} Interview</h3>
+          <div className='flex flex-row gap-5 mt-3'>
+            <div className='flex flex-row gap-2'>
+              <Image src='/calendar.svg' alt='Calendar' width={21} height={21} />
+              <p>{formattedDate}</p>
+            </div>
+            <div className='flex flex-row gap-2'>
+              <Image src='/star.svg' alt='Star' width={22} height={22} /> 
+              <p>{ feedback?.totalScore || '---' }/100</p>
+            </div>
+          </div>
+          <p className='line-clamp-2 mt-5'>
+            {feedback?.finalAssessment || "You haven't taken the interview yet. Take it now to improve your skills."}
+          </p>
+        </div>
+        <div className='flex flex-row justify-between'>
+          <DisplayTechIcons techStack={techstack} />
+          <Button className='btn-primary'>
+            <Link href={feedback ? `/interview/${interviewId}/feedback` : `/interview/${interviewId}`}>
+              {feedback ? 'Check Feedback' : 'View Interview'}
+            </Link>
+          </Button>
         </div>
       </div>
     </div>
---
+++ Practical/firebase/client.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCuZH_jB8_DMIaCnI0Gki8aMuK_kwV5oE4",
+  authDomain: "practical-4c3d9.firebaseapp.com",
+  projectId: "practical-4c3d9",
+  storageBucket: "practical-4c3d9.firebasestorage.app",
+  messagingSenderId: "781290768020",
+  appId: "1:781290768020:web:47ffc7c13a573a0cb39681",
+  measurementId: "G-WDV6LJNYK9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
